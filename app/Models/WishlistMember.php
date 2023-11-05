8599bb5d440dfd847175d73eb4a0f5a50d46705f
Hoang Ha <Hoangha2352@gmail.com>
2023-11-06 00:14:12 +0700

    first commit

---
+++ app/Models/WishlistMember.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WishlistMember extends Model
+{
+    use HasFactory;
+
+    protected $table = 'wishlistmember';
+
+    protected $fillable = [
+        'id',
+        'id_member',
+        'id_tutor'
+    ];
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class,'id_member');
+    }
+
+    public function tutor()
+    {
+        return $this->belongsTo(Tutor::class,'id_tutor');
+    }
+}
