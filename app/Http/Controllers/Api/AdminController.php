601e0b48af8f1663b1c622c73478b73dee5f1400
Hoang Ha <Hoangha2352@gmail.com>
2023-11-13 02:17:19 +0700

    update

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function login(Request $request)
     {
-        
+        $data = $request->json()->all();
+
+        $login = [
+            'email' => $data['email'],
+            'password' => $data['password']
+        ];
+        if(Auth::attempt($login)){
+            return response()->json(['success'=>'Đăng nhập thành công']);
+        }else{
+            return response()->json(['errors'=>'Đăng nhập thất bại']);
+        }
     }
 }
